6e52cded6717e086780d47d9764bd8c353059657
Kirti Solanki <allmywork360@gmail.com>
2026-07-15 22:57:34 +0530

    grid

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ arr.forEach(button => {
             string = "";
             input.value = string;
           }
-          else if ( e.target.innerHTML == 'DEL' ){
+          else if ( (button.dataset.action === "delete") ){
               string = string.substring(0, string.
               length-1);
               input.value = string;
